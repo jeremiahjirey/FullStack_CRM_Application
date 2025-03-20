30e2f1b2cc8c70e854652003b468c988014c183e
howlcart <imannuelsmkn1bms@gmail.com>
2025-03-21 01:17:04 +0700

    Create Dockefile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,35 +18,35 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::create(['ussername'=>'superadmin','email'=>'superadmin@admin.com','password'=>'password','role'=>'superadmin']);    
-        // User::create(['ussername'=>'admin','email'=>'admin@admin.com','password'=>'password','role'=>'admin']);    
-        // User::create(['ussername'=>'user','email'=>'user@user.com','password'=>'password','role'=>'user']);    
+        User::create(['ussername'=>'superadmin','email'=>'superadmin@admin.com','password'=>'password','role'=>'superadmin']);    
+        User::create(['ussername'=>'admin','email'=>'admin@admin.com','password'=>'password','role'=>'admin']);    
+        User::create(['ussername'=>'user','email'=>'user@user.com','password'=>'password','role'=>'user']);    
 
-        // Companie::create(['name'=>'PT Brailing Inovasi Teknologi','email'=>'bitkreasi@sch.id','logo'=>'http://logo','website'=>'https://bitkreasi.com']); 
+        Companie::create(['name'=>'PT Brailing Inovasi Teknologi','email'=>'bitkreasi@sch.id','logo'=>'http://logo','website'=>'https://bitkreasi.com']);
 
-        // Employee::create([
-        //     'first_name' => 'John',
-        //     'last_name' => 'Doe',
-        //     'company_id' => 1,
-        //     'email' => 'john.doe@example.com',
-        //     'phone' => '1234567890',
-        // ]);
+        Employee::create([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'company_id' => 1,
+            'email' => 'john.doe@example.com',
+            'phone' => '1234567890',
+        ]);
 
-        // Employee::create([
-        //     'first_name' => 'Maulana',
-        //     'last_name' => 'Ibnu',
-        //     'company_id' => 1,
-        //     'email' => 'ibnsaban@example.com',
-        //     'phone' => '1234567890',
-        // ]);
+        Employee::create([
+            'first_name' => 'Maulana',
+            'last_name' => 'Ibnu',
+            'company_id' => 1,
+            'email' => 'ibnsaban@example.com',
+            'phone' => '1234567890',
+        ]);
 
-        // Employee::create([
-        //     'first_name' => 'Rizki',
-        //     'last_name' => 'Ramadhan',
-        //     'company_id' => 1,
-        //     'email' => 'Rizki@example.com',
-        //     'phone' => '1234567890',
-        // ]);
+        Employee::create([
+            'first_name' => 'Rizki',
+            'last_name' => 'Ramadhan',
+            'company_id' => 1,
+            'email' => 'Rizki@example.com',
+            'phone' => '1234567890',
+        ]);
 
         Division::create([
             'name_division' => 'Front End',
@@ -60,20 +60,20 @@ class DatabaseSeeder extends Seeder
 
         Division::create([
             'name_division' => 'Engginer',
-            'company_id' => 2
+            'company_id' => null,
         ]);
 
-        // $division = Division::where('name_division', 'Back End')->first();
+        $division = Division::where('name_division', 'Back End')->first();
 
-    // // Tentukan karyawan yang akan dimasukkan ke dalam divisi
-    //     $employeeIds = [3];
+    // Tentukan karyawan yang akan dimasukkan ke dalam divisi
+        $employeeIds = [1];
 
-    // // Update karyawan dengan menetapkan divisi_id
-    //     foreach ($employeeIds as $employeeId) {
-    //         $employee = Employee::find($employeeId);
-    //         $employee->division_id = $division->id;
-    //         $employee->save();
-    //     }
+    // Update karyawan dengan menetapkan divisi_id
+        foreach ($employeeIds as $employeeId) {
+            $employee = Employee::find($employeeId);
+            $employee->division_id = $division->id;
+            $employee->save();
+        }
 
     }
 }
